df9388e72427e38640b5e166545190365fe96903
jonatanlaksamanapurnomo <jonatanlaksamanapurnomo@gmail.com>
2023-11-18 16:01:29 +0700

    Add interactive cli

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/manifoldco/promptui"
 	"log"
+	"strconv"
 	"toped-scrapper/domain/product"
 	"toped-scrapper/pkg/database/postgres"
 	ucproduct "toped-scrapper/usecase/product"
@@ -17,12 +19,61 @@ var (
 func main() {
 	Init()
 
+	// Interactive prompts
+	category := promptForInput("Enter product category (e.g., handphone): ")
+	limit := promptForNumber("Enter limit of products to fetch (e.g., 100): ")
+	worker := promptForNumber("Enter number of workers (e.g., 5): ")
+
 	products, err := productUsecase.GetTokopediaProduct(context.Background(), ucproduct.GetProductParam{
-		Category: "handphone",
-		Limit:    100,
-		Worker:   5,
+		Category: category,
+		Limit:    limit,
+		Worker:   worker,
 	})
-	fmt.Println(len(products), err)
+	if err != nil {
+		log.Fatalf("Error fetching products: %v", err)
+	}
+
+	fmt.Printf("\nFetched %d products\n", len(products))
+	fmt.Printf("Check outputs folder for csv \n")
+
+	for _, p := range products {
+		fmt.Printf("Name: %s, Price: %s\n", p.Name, p.Price)
+		// Add more details if needed
+	}
+}
+
+func promptForInput(label string) string {
+	prompt := promptui.Prompt{
+		Label: label,
+	}
+	result, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+	return result
+}
+
+func promptForNumber(label string) int {
+	validate := func(input string) error {
+		_, err := strconv.Atoi(input)
+		if err != nil {
+			return fmt.Errorf("invalid number")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    label,
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+
+	number, _ := strconv.Atoi(result)
+	return number
 }
 
 func Init() {
--- go.mod
+++ go.mod
@@ -12,11 +12,13 @@ require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/chromedp/cdproto v0.0.0-20231011050154-1d073bb38998 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.3.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
